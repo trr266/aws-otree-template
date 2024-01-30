a56e88200c13cfb5cc96c28360efdece7b93facf
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-01-30 12:36:35 +0000

    add settings, expand readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # aws-otree-template
 Template for deploying oTree to AWS
+
+
+## Acknowledgements
+
+Uses template code from https://github.com/oTree-org/oTree, uses oTree.
+
+
+## Citations
+
+Chen, Daniel L., Martin Schonger, and Chris Wickens.
+2016. "oTree - An open-source platform for laboratory, online, and field experiments."
+Journal of Behavioral and Experimental Finance, vol 9: 88-97.
--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,58 @@
-# Test test
+# Demo settings.py, Source: https://github.com/oTree-org/oTree/blob/lite/settings.py
+
+from os import environ
+
+
+SESSION_CONFIGS = [
+    dict(
+        name='guess_two_thirds',
+        display_name="Guess 2/3 of the Average",
+        app_sequence=['guess_two_thirds', 'payment_info'],
+        num_demo_participants=3,
+    ),
+    dict(
+        name='survey', app_sequence=['survey', 'payment_info'], num_demo_participants=1
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+
+SECRET_KEY = '{{ secret_key }}'
+
+INSTALLED_APPS = ['otree']
